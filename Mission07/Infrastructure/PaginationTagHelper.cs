4b6a298b28afdfd2660850111c5d4cfb7a9a6dac
Jacob Poor <jacob.poor98@gmail.com>
2022-02-23 10:18:49 -0700

    Yes

---
+++ Mission07/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission07.Models.ViewModels;
+
+namespace Mission07.Infrastructure
+{
+    // this is used to create the page-book shortcut so that the html for
+    // the number of pages needed is dynamically created
+    [HtmlTargetElement("div", Attributes = "page-book")]
+    public class PaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        public PageInfo PageBook { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            // this specifically will add the HTML with links
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageBook.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
